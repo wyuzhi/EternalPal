c040a328b97d3eecc85a34dabdc2c1f59f6ebae4
xuann <tonyxuann@outlook.com>
2025-09-09 20:07:11 +0800

    feat: 优化宠物档案和关于页面

--- pages/settings/info/info.js
+++ pages/settings/info/info.js
@@ -3,14 +3,23 @@ const app = getApp();
 Page({
   data: {
     appInfo: {
-      name: '灵伴',
+      name: '灵伴 Eternal Pal',
       version: 'v 0.1.0',
-      description: '灵伴是一款AI原生的数字宠物情感陪伴应用，通过AI技术重现用户的宠物形象，实现对话互动与情感链接，灵伴让逝去或缺席的陪伴得以延续，为用户带来真实而温暖的情感链接。',
+      description: '灵伴（Eternal Pal/E Pal）是一款AI原生的数字宠物情感陪伴应用，通过AI技术重现用户的宠物形象，实现对话互动与情感链接，灵伴让逝去或缺席的陪伴得以延续，为用户带来真实而温暖的情感链接。',
       logo: '/images/logo.svg',
       features: [
-        '情感陪伴系统',
-        'AI对话',
-        '宠物形象重建',
+        {
+          name: '宠物形象重建',
+          backgroundImage: '/images/settings/il3.svg'
+        },
+        {
+          name: '情感陪伴',
+          backgroundImage: '/images/settings/il1.svg'
+        },
+        {
+          name: '互动对话',
+          backgroundImage: '/images/settings/il2.svg'
+        },
       ]
     },
     timeline: [
